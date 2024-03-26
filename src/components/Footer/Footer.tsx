6b517382ecb86bd19865a9b28a1f72a7f6fa6ee3
qooxx <bernardopvsouza@gmail.com>
2024-03-26 15:55:25 -0300

    feat: address number and new desktop breakpoint for global units

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,12 @@
 import './Footer.css'
-import IconList from '../ui/IconList/IconList'
-
-import email from '../../assets/icons/email.svg'
-import zap from '../../assets/icons/zap.svg'
-import insta from '../../assets/icons/insta.svg'
 
 const Footer = () => {
-  const contactInfo = [
-    {
-      title: 'contato@precminas.com.br',
-      iconPath: email,
-    },
-    {
-      title: '+55 (31) 99103-5736',
-      iconPath: zap,
-    },
-    {
-      title: '@precminas',
-      iconPath: insta,
-    },
-  ]
-
   return (
     <footer className='flex'>
       <p className='fw-semi-bold text-white'>
         <span className='fw-semi-bold'>PRECMINAS</span> | 2024 © Todos os
         direitos reservados
       </p>
-      <IconList items={contactInfo} orientation='row'></IconList>
     </footer>
   )
 }
