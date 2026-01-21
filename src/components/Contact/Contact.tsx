7da717dcccb7e6f8c2f2ec1d59efef35ba916e09
bernardo-pernes <bernardo.souza@petrus-software.com>
2026-01-21 20:49:28 -0300

    hotfix: room number

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -8,41 +8,41 @@ import map_pin from '../../assets/icons/map-pin.svg'
 import phone from '../../assets/icons/phone.svg'
 
 const Contact = () => {
-  const contactInfo = [
-    {
-      title: 'contato@precminas.com.br',
-      iconPath: email,
-    },
-    {
-      title: '+55 (31) 99103-5736',
-      iconPath: zap,
-    },
-    {
-      title: '@precminas',
-      iconPath: insta,
-    },
-    {
-      title: 'Rua Timbiras, 1940 - Sala 1714 - Lourdes - Belo Horizonte',
-      iconPath: map_pin,
-    },
-    {
-      title: '31 3058-9844',
-      iconPath: phone,
-    },
-  ]
+	const contactInfo = [
+		{
+			title: 'contato@precminas.com.br',
+			iconPath: email,
+		},
+		{
+			title: '+55 (31) 99103-5736',
+			iconPath: zap,
+		},
+		{
+			title: '@precminas',
+			iconPath: insta,
+		},
+		{
+			title: 'Rua Timbiras, 1940 - Sala 1810 - Lourdes - Belo Horizonte',
+			iconPath: map_pin,
+		},
+		{
+			title: '31 3058-9844',
+			iconPath: phone,
+		},
+	]
 
-  return (
-    <section id='contato' className='contact-section | section-spacing'>
-      <h2 className='fs-h3 text-white'>Contato</h2>
-      <p className='fs-body fs-title text-white'>
-        Se você está pronto para explorar o vasto mundo das oportunidades, entre
-        em contato conosco. Nossa equipe especializada está aqui para responder
-        às suas perguntas, entender suas necessidades e guiar você.
-      </p>
-      <ContactForm />
-      <IconList items={contactInfo} orientation='row'></IconList>
-    </section>
-  )
+	return (
+		<section id='contato' className='contact-section | section-spacing'>
+			<h2 className='fs-h3 text-white'>Contato</h2>
+			<p className='fs-body fs-title text-white'>
+				Se você está pronto para explorar o vasto mundo das oportunidades, entre
+				em contato conosco. Nossa equipe especializada está aqui para responder
+				às suas perguntas, entender suas necessidades e guiar você.
+			</p>
+			<ContactForm />
+			<IconList items={contactInfo} orientation='row'></IconList>
+		</section>
+	)
 }
 
 export default Contact
